643f55d562881ad47ac50aeb9ddc2b02be8a73ca
Jan Graichen <jg@altimos.de>
2014-04-27 13:09:14 +0200

    Release 0.3.0

--- lib/rubypath/version.rb
+++ lib/rubypath/version.rb
@@ -1,8 +1,8 @@
 class Path
   module VERSION
     MAJOR = 0
-    MINOR = 2
-    PATCH = 1
+    MINOR = 3
+    PATCH = 0
     STAGE = nil
     STRING = [MAJOR, MINOR, PATCH, STAGE].reject(&:nil?).join('.').freeze
 
